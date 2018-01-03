60aa4d171424fb73215aae343f4e607a3e688a85
Mladen <djoml8bre@gmail.com>
2018-01-04 00:57:26 +0100

    Commit-2 Creating App Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShopingListComponent } from './shoping/shoping-list/shoping-list.component';
+import { ShopingListEditComponent } from './shoping/shoping-list/shoping-list-edit/shoping-list-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopingListComponent,
+    ShopingListEditComponent,
+    RecipeListComponent,
+    RecipesComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shoping-list/shoping-list-edit/shoping-list-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoping-list-edit',
+  templateUrl: './shoping-list-edit.component.html',
+  styleUrls: ['./shoping-list-edit.component.css']
+})
+export class ShopingListEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
