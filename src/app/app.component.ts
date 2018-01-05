b96f0b7cc2cb3d597ed70b23ce7822832310fa1b
Mladen <djoml8bre@gmail.com>
2018-01-05 21:34:22 +0100

    Commit-11 Adding Navigation with Event Binding and *ngIf - My Solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  recipes: boolean;
+  shoppingList: boolean;
+
+  showingRecipes(value) {
+    this.recipes = value;
+    this.shoppingList = false;
+  }
+
+  showingShoppingList(value) {
+    this.shoppingList = value;
+    this.recipes = false;
+  }
 }
