d16fb0fdcf6ad37f5876e0033601cf772d07f22c
Mladen <djoml8bre@gmail.com>
2018-01-27 11:52:16 +0100

    Commit-61 Sending the Token to Firebase Backend

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,20 +3,26 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from './../recipes/recipe.model';
+import { AuthService } from './../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
 
   constructor(private http: Http,
-              private recipeService: RecipeService) {}
+              private recipeService: RecipeService,
+              private authService: AuthService) {}
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-book-7466b.firebaseio.com/recipes.json',
+    const token = this.authService.getToken();
+
+    return this.http.put('https://ng-recipe-book-7466b.firebaseio.com/recipes.json?auth=' + token,
       this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    return this.http.get('https://ng-recipe-book-7466b.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+
+    this.http.get('https://ng-recipe-book-7466b.firebaseio.com/recipes.json?auth=' + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
