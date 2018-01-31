0a7b0d4adf43fbb0f9dab7f447b6e45cb547ddcc
Mladen <djoml8bre@gmail.com>
2018-01-31 11:36:53 +0100

    Commit-84 Modifying Requests in Interceptors, Setting Auth Parameters

--- src/app/shared/auth.interseptor.ts
+++ src/app/shared/auth.interseptor.ts
@@ -1,9 +1,17 @@
+import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from './../auth/auth.service';
 
+@Injectable()
 export class AuthInterseptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted!', request);
-    return next.handle(request);
+    // const copiedReq = request.clone({headers: request.headers.set('', '')});
+    const copiedRequest = request.clone({params: request.params.set('auth', this.authService.getToken())});
+    return next.handle(copiedRequest);
   }
 }
